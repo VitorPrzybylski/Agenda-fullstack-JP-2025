4d20016754e1e4b9bede6fbdb31ad866b5961b57
VitorPrzybylski <vitorbibow@gmail.com>
2025-11-28 17:28:40 -0300

    feat

--- backend/src/router/clientes.js
+++ backend/src/router/clientes.js
@@ -1,20 +1,16 @@
 import express from 'express'
 import ControllerCliente from "../controller/clientes.js"
-import authMiddleware from "../middleware/auth.js"
 
 const routerCliente = express.Router()
 
+// LOGIN
 routerCliente.post('/login', ControllerCliente.Login)
 
-
-routerCliente.get('/cliente/context',authMiddleware(), ControllerCliente.FindOne)
+// CRUD SEM PERMISSÃO
+routerCliente.get('/clientes', ControllerCliente.FindAll)
+routerCliente.get('/cliente/:id', ControllerCliente.FindOne)
 routerCliente.post('/cliente/create', ControllerCliente.Create)
-routerCliente.put('/cliente/',authMiddleware(), ControllerCliente.Update)
-routerCliente.delete('/cliente/',authMiddleware(), ControllerCliente.Delete)
+routerCliente.put('/cliente/:id', ControllerCliente.Update)
+routerCliente.delete('/cliente/:id', ControllerCliente.Delete)
 
-routerCliente.get('/clientes',authMiddleware([0]), ControllerCliente.FindAll)
-routerCliente.get('/cliente/:id',authMiddleware([0]), ControllerCliente.FindOne)
-routerCliente.post('/cliente/admin',authMiddleware([0]), ControllerCliente.Create)
-routerCliente.put('/cliente/:id',authMiddleware([0]), ControllerCliente.Update)
-routerCliente.delete('/cliente/:id',authMiddleware([0]), ControllerCliente.Delete)
 export default routerCliente
--- frontend/src/auth/Context.jsx
+++ frontend/src/auth/Context.jsx
@@ -1,62 +1,23 @@
-import { createContext, useEffect, useState } from "react";
-import { jwtDecode } from 'jwt-decode'
-
-const isTokenValid = (token) => {
-    try {
-        console.log(token)
-        const decoded = jwtDecode(token)
-        const currentTime = Date.now() / 1000
-        return decoded.exp > currentTime
-    } catch (error) {
-        return false
-    }
-}
-
-const getRole = (token) => {
-    try {
-        const decoded = jwtDecode(token)
-        return decoded.role
-    } catch (error) {
-        return false
-    }
-}
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext()
-// token do usuario
-export const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState(null)
-    const [role, setRole] = useState(null)
-    const [loading, setLoading] = useState(true)
 
-    const login = (token) => {
-        setToken(token)
-        localStorage.setItem('token', token)
+export const AuthProvider = ({ children }) => {
+    const [cliente, setCliente] = useState(null)
+
+    const login = (cliente) => {
+        setCliente(cliente)
+        localStorage.setItem("cliente", JSON.stringify(cliente))
     }
 
     const logout = () => {
-        setToken(null)
-        localStorage.removeItem('token')
+        setCliente(null)
+        localStorage.removeItem("cliente")
     }
 
-    useEffect(() => {
-        const storageToken = localStorage.getItem('token')
-        if (storageToken && isTokenValid(storageToken)) {
-            setToken(storageToken)
-            setRole(getRole(storageToken))
-        } else {
-            setToken(null)
-            setRole(null)
-            localStorage.removeItem('token')
-        }
-        setLoading(false)
-    }, [])
-
-    if(loading) {
-        return <div>Carregando...</div>
-    }
     return (
-        <AuthContext.Provider value={{ token, login, logout, role }}>
-            { children }
+        <AuthContext.Provider value={{ cliente, login, logout }}>
+            {children}
         </AuthContext.Provider>
     )
 }
