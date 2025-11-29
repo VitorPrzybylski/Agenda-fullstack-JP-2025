0635c43a73343c70c2f8a6203ba1d64691cc3927
VitorPrzybylski <vitorbibow@gmail.com>
2025-11-28 22:16:50 -0300

    fat

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.baseURL ??  "http://localhost:3000",
-    
+    // baseURL: import.meta.env.baseURL ??  "http://localhost:3000",
+    baseURL: "https://agenda-fullstack-jp-2025.onrender.com"
 })
 
 export default api;
